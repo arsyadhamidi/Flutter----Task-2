91e5975663f9d9357f653faec9741ea9fdd828a9
arsyadhamidi <arsyadhamidi04@gmail.com>
2023-02-21 16:27:06 +0700

    initial commit

--- lib/registrasi.dart
+++ lib/registrasi.dart
@@ -23,6 +23,7 @@ class _RegistrasiPageState extends State<RegistrasiPage> {
     }
   }
 
+  GlobalKey<FormState> _key = GlobalKey();
   String gender = 'male';
   List<String> _item = ['Islam', 'Kristen', 'Budha', 'Hindu'];
   String _selectItem = 'Islam';
@@ -30,6 +31,14 @@ class _RegistrasiPageState extends State<RegistrasiPage> {
   TextEditingController _nama = TextEditingController();
   TextEditingController _password = TextEditingController();
   TextEditingController _motto = TextEditingController();
+
+  void validasiRegistrasi(){
+    if(_key.currentState!.validate()){
+      if(_nama.text.isEmpty && _password.text.isEmpty && _motto.text.isEmpty){
+        print("Data Tidak Boleh Kosong");
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,140 +51,159 @@ class _RegistrasiPageState extends State<RegistrasiPage> {
           ),
         ),
       ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
-            child: TextFormField(
-              controller: _nama,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(CupertinoIcons.person),
-                  label: Text("Masukan Nama Lengkap"),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(),
-                  )),
+      body: Form(
+        key: _key,
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+              child: TextFormField(
+                controller: _nama,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(CupertinoIcons.person),
+                    label: Text("Masukan Nama Lengkap"),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(),
+                    )),
+                validator: (e) {
+                  if(e!.isEmpty){
+                    return "Data Nama Tidak Boleh Kosong!";
+                  }
+                },
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
-            child: TextFormField(
-              controller: _password,
-              obscureText: _obsure,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(CupertinoIcons.padlock),
-                  label: Text("Masukan Password"),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(),
-                  ),
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      _cekPassword();
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+              child: TextFormField(
+                controller: _password,
+                obscureText: _obsure,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(CupertinoIcons.padlock),
+                    label: Text("Masukan Password"),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        _cekPassword();
+                      },
+                      icon: Icon(_obsure
+                          ? CupertinoIcons.eye_slash
+                          : CupertinoIcons.eye),
+                    )),
+                validator: (e){
+                  if(e!.isEmpty){
+                    return "Data Password Tidak Boleh Kosong!";
+                  }
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
+              child: TextFormField(
+                maxLines: 8,
+                controller: _motto,
+                decoration: InputDecoration(
+                    hintText: 'Masukkan Motto Hidup',
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(),
+                    )),
+                validator: (e){
+                  if(e!.isEmpty){
+                    return "Data Motto Tidak Boleh Kosong!";
+                  }
+                },
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: RadioListTile(
+                    value: 'male',
+                    title: Text("Male"),
+                    groupValue: gender,
+                    onChanged: (value) {
+                      setState(() {
+                        gender = value.toString();
+                      });
                     },
-                    icon: Icon(_obsure
-                        ? CupertinoIcons.eye_slash
-                        : CupertinoIcons.eye),
-                  )),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 10, right: 10),
-            child: TextFormField(
-              maxLines: 8,
-              controller: _motto,
-              decoration: InputDecoration(
-                  hintText: 'Masukkan Motto Hidup',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(),
-                  )),
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: RadioListTile(
-                  value: 'male',
-                  title: Text("Male"),
+                  ),
+                ),
+                Expanded(
+                    child: RadioListTile(
+                  value: 'female',
+                  title: Text("Female"),
                   groupValue: gender,
                   onChanged: (value) {
                     setState(() {
                       gender = value.toString();
                     });
                   },
-                ),
-              ),
-              Expanded(
-                  child: RadioListTile(
-                value: 'female',
-                title: Text("Female"),
-                groupValue: gender,
+                ))
+              ],
+            ),
+            Center(
+              child: Text("Agama :"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: DropdownButton(
+                value: _selectItem,
+                icon: Icon(Icons.arrow_drop_down_outlined),
                 onChanged: (value) {
                   setState(() {
-                    gender = value.toString();
+                    _selectItem = value!;
                   });
                 },
-              ))
-            ],
-          ),
-          Center(
-            child: Text("Agama :"),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-            child: DropdownButton(
-              value: _selectItem,
-              icon: Icon(Icons.arrow_drop_down_outlined),
-              onChanged: (value) {
-                setState(() {
-                  _selectItem = value!;
-                });
-              },
-              items: _item.map<DropdownMenuItem<String>>((String dataAgama) {
-                return DropdownMenuItem(
-                  value: dataAgama,
-                  child: Text(dataAgama),
-                );
-              }).toList(),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: MaterialButton(
-              height: 60,
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: Center(
-                            child: Text(
-                          "INFORMASI",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )),
-                        content: Container(
-                          height: 200,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text("Nama : ${_nama.text}"),
-                              Text("Password : ${_password.text}"),
-                              Text("Motto : ${_motto.text}"),
-                              Text("Jenis Kelamin : ${gender}"),
-                              Text("Agama : ${_selectItem}"),
-                            ],
-                          ),
-                        ),
-                      );
-                    });
-              },
-              color: Colors.blueAccent,
-              child: Text(
-                "SIGN UP",
-                style: TextStyle(color: Colors.white),
+                items: _item.map<DropdownMenuItem<String>>((String dataAgama) {
+                  return DropdownMenuItem(
+                    value: dataAgama,
+                    child: Text(dataAgama),
+                  );
+                }).toList(),
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: MaterialButton(
+                height: 60,
+                onPressed: () {
+                  validasiRegistrasi();
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Center(
+                              child: Text(
+                            "INFORMASI",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          )),
+                          content: Container(
+                            height: 200,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text("Nama : ${_nama.text}"),
+                                Text("Password : ${_password.text}"),
+                                Text("Motto : ${_motto.text}"),
+                                Text("Jenis Kelamin : ${gender}"),
+                                Text("Agama : ${_selectItem}"),
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                },
+                color: Colors.blueAccent,
+                child: Text(
+                  "SIGN UP",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
